edf4b3682582f825d21b2a163359b34347adb08b
AbrOuma <braham0369@gmail.com>
2024-06-02 09:00:00 +0300

    test

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,7 +7,7 @@ environ.Env.read_env(BASE_DIR / '.env', overwrite=True)
 
 SECRET_KEY    = env('SECRET_KEY')
 DEBUG         = False
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': env.db('DATABASE_URL')
